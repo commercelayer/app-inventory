7b3f26031910a295edeab1e9a335abdff4007f60
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-02-06 12:25:27 +0100

    chore: refine the UI of sku selection overlay

--- packages/app/src/hooks/useAddItemOverlay.tsx
+++ packages/app/src/hooks/useAddItemOverlay.tsx
@@ -1,6 +1,11 @@
 import { ListEmptyState } from '#components/ListEmptyState'
 import { ListItemSku } from '#components/ListItemSku'
-import { useOverlay, useResourceFilters } from '@commercelayer/app-elements'
+import {
+  Card,
+  PageLayout,
+  useOverlay,
+  useResourceFilters
+} from '@commercelayer/app-elements'
 import type { FiltersInstructions } from '@commercelayer/app-elements/dist/ui/resources/useResourceFilters/types'
 import type { Sku } from '@commercelayer/sdk'
 import { navigate, useSearch } from 'wouter/use-location'
@@ -38,61 +43,75 @@ export function useAddItemOverlay(): OverlayHook {
 
       return (
         <OverlayElement backgroundColor='light'>
-          <div className='w-full flex items-center gap-4'>
-            <div className='flex-1'>
-              <SearchWithNav
-                onFilterClick={() => {}}
-                onUpdate={(qs) => {
-                  navigate(`?${qs}`, {
-                    replace: true
-                  })
-                }}
-                queryString={queryString}
-                hideFiltersNav
-                searchBarPlaceholder='search...'
-              />
-            </div>
-            <div className='mt-4 mb-14'>
-              <button
-                onClick={() => {
-                  close()
-                }}
-                className='text-primary font-bold rounded px-1 shadow-none !outline-0 !border-0 !ring-0 focus:shadow-focus'
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-
-          <FilteredList
-            type='skus'
-            query={{
-              fields: {
-                customers: [
-                  'id',
-                  'email',
-                  'total_orders_count',
-                  'created_at',
-                  'updated_at',
-                  'customer_group'
-                ]
-              }
+          <PageLayout
+            title='Pick a SKU'
+            gap='only-top'
+            navigationButton={{
+              onClick: () => {
+                close()
+              },
+              label: 'Back',
+              icon: 'arrowLeft'
             }}
-            ItemTemplate={(props) => (
-              <ListItemSku
-                variant='list'
-                onSelect={(resource) => {
-                  onConfirm(resource)
-                  close()
-                  navigate(`?`, {
-                    replace: true
-                  })
+          >
+            <div className='w-full flex items-center gap-4'>
+              <div className='flex-1'>
+                <SearchWithNav
+                  onFilterClick={() => {}}
+                  onUpdate={(qs) => {
+                    navigate(`?${qs}`, {
+                      replace: true
+                    })
+                  }}
+                  queryString={queryString}
+                  hideFiltersNav
+                  searchBarPlaceholder='search...'
+                />
+              </div>
+              <div className='mt-4 mb-14'>
+                <button
+                  onClick={() => {
+                    close()
+                  }}
+                  className='text-primary font-bold rounded px-1 shadow-none !outline-0 !border-0 !ring-0 focus:shadow-focus'
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+            <Card gap='none'>
+              <FilteredList
+                type='skus'
+                query={{
+                  fields: {
+                    customers: [
+                      'id',
+                      'email',
+                      'total_orders_count',
+                      'created_at',
+                      'updated_at',
+                      'customer_group'
+                    ]
+                  }
                 }}
-                {...props}
+                ItemTemplate={(props) => (
+                  <ListItemSku
+                    variant='list'
+                    onSelect={(resource) => {
+                      onConfirm(resource)
+                      close()
+                      navigate(`?`, {
+                        replace: true
+                      })
+                    }}
+                    {...props}
+                  />
+                )}
+                emptyState={<ListEmptyState />}
+                hideTitle
               />
-            )}
-            emptyState={<ListEmptyState />}
-          />
+            </Card>
+          </PageLayout>
         </OverlayElement>
       )
     }
